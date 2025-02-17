f2531e174b8784e96dcdc0577d4ff2d68da93316
serzhio <mahov.serezha2015@gmail.com>
2025-02-18 00:16:28 +0300

    first commit

---
+++ src/main/java/com/serzhio_pet_projects/textum/TextumApplication.java
@@ -0,0 +1,13 @@
+package com.serzhio_pet_projects.textum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextumApplication.class, args);
+	}
+
+}
